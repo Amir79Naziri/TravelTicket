73d7d9ae73083046ec7090a80a08b0359b907709
Amir79Naziri <naziriamirreza@gmail.com>
2021-02-01 20:26:18 +0330

    "Complete Login1"

--- src/sample/login/Main.java
+++ src/sample/login/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/sample/login/PhoneLoginController.java
+++ src/sample/login/PhoneLoginController.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.login;
 
 public class PhoneLoginController {
 }
