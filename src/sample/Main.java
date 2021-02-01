a74ec6dcead0f18500f7532581c239948ef0dd04
Amir79Naziri <naziriamirreza@gmail.com>
2021-02-01 12:59:15 +0330

    "init"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("loginView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello  World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
