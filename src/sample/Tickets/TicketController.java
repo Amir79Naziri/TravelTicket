5ac224cab92b90ca5432fef19217e91f794b2d64
Faraz <f.farangizadeh@gmail.com>
2021-02-02 14:00:38 +0330

    "Add Price to tickets"

--- src/sample/Tickets/TicketController.java
+++ src/sample/Tickets/TicketController.java
@@ -1,8 +1,10 @@
 package sample.Tickets;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.Random;
@@ -13,6 +15,16 @@ public class TicketController  implements Initializable
 
     @FXML
     private Label cost;
+
+    @FXML
+    private JFXButton purchaseButton;
+
+    @FXML
+    void purchase(MouseEvent event)
+    {
+        System.out.println(cost.getText());
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
