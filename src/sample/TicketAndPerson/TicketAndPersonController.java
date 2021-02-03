b298b2ea54c62ef8455c30c770f2d76ac467187e
amir <amirsabbagh2000z@gmail.com>
2021-02-03 12:09:23 +0330

    fix train And Airplain icon changer in controller

--- src/sample/TicketAndPerson/TicketAndPersonController.java
+++ src/sample/TicketAndPerson/TicketAndPersonController.java
@@ -8,6 +8,45 @@ import javafx.scene.layout.Pane;
 public class TicketAndPersonController {
 
 
+    @FXML
+    private Pane inTrain;
+
+    @FXML
+    private Pane outTrain;
+
+    @FXML
+    private ImageView outPlane;
+
+    @FXML
+    private ImageView inPlane;
+
+    @FXML
+    private JFXCheckBox TrainCheckBox;
+
+    @FXML
+    private JFXCheckBox AirplaneCheckBox;
+
+    @FXML
+    void trainCheckBoxHandler() {
+        if (TrainCheckBox.isSelected())
+            AirplaneCheckBox.setSelected(false);
+        inPlane.setVisible(false);
+        outPlane.setVisible(false);
+        inTrain.setVisible(true);
+        outTrain.setVisible(true);
+
+    }
+
+    @FXML
+    void airplaneCheckBoxHandler() {
+        if (AirplaneCheckBox.isSelected())
+            TrainCheckBox.setSelected(false);
+        inPlane.setVisible(true);
+        outPlane.setVisible(true);
+        inTrain.setVisible(false);
+        outTrain.setVisible(false);
+
+    }
 
 //    @FXML
 //    void blurairplane(ActionEvent event) {
