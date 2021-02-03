70611df55d36b4e96b93da784a212b303d679826
amir <amirsabbagh2000z@gmail.com>
2021-02-03 08:57:29 +0330

    add ticket&person and search pages

---
+++ src/sample/Search/SearchController.java
@@ -0,0 +1,4 @@
+package sample.Search;
+
+public class SearchController {
+}
---
+++ src/sample/TicketAndPerson/TicketAndPersonController.java
@@ -0,0 +1,32 @@
+package sample.TicketAndPerson;
+
+import com.jfoenix.controls.JFXCheckBox;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class TicketAndPersonController {
+
+
+
+//    @FXML
+//    void blurairplane(ActionEvent event) {
+//        origintrain.setVisible(false);
+//        origintrain.setVisible(false);
+//        originairplain.setVisible(true);
+//        destairplane.setVisible(true);
+//    }
+
+//    @FXML
+//    void blurtrain(ActionEvent event) {
+//        originairplain.setVisible(false);
+//        destairplane.setVisible(false);
+//        origintrain.setVisible(true);
+//        origintrain.setVisible(true);
+//    }
+
+
+
+
+
+}
