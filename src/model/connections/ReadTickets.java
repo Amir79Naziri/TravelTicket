a6ac49e2948ba598d4f7918cbb2fdaf1fa51cd3e
Faraz <f.farangizadeh@gmail.com>
2021-02-06 17:13:52 +0330

    "Add retrieved ticket to available tickets"

---
+++ src/model/connections/ReadTickets.java
@@ -0,0 +1,20 @@
+package model.connections;
+
+import model.FileHandler;
+import model.Ticket;
+import model.TicketStorage;
+
+import java.util.Map;
+
+public class ReadTickets
+{
+    public static void main(String[] arg)
+    {
+        TicketStorage ticketStorage = (TicketStorage) FileHandler.load("Files/ticketStorage.ser");
+        for (Map.Entry<Integer, Ticket> set : ticketStorage.getTickets().entrySet())
+        {
+            Ticket temp = set.getValue();
+            System.out.println(temp.toString());
+        }
+    }
+}
